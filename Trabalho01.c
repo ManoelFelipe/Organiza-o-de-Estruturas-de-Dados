30271a8bf25bd3ff3c3842be1e8f73460ec31bdc
Manoel Felipe Costa Furtado <manoel.furtado@eic.cefet-rj.br>
2018-03-27 02:36:28 -0300

    Update Trabalho01.c

--- Trabalho01.c
+++ Trabalho01.c
@@ -17,15 +17,15 @@ int main(int argc, char **argv) {
 	FILE *f;
 	Endereco e;
 	int cont = 1;
-    long posicao, primeiro, ultimo, meio;
+	long posicao, primeiro, ultimo, meio;
 
 	if(argc != 2) {
-        fprintf(stderr,"Erro na chamada do programa.\n");
+    fprintf(stderr,"Erro na chamada do programa.\n");
 		fprintf(stderr, "Aceito Somente 1 argumento: [CEP] \n");
 		return 1;
 	}
 	f = fopen("cep_ordenado.dat","r");
-    if(!f) {
+  if(!f) {
 		fprintf(stderr,"Arquivo não pode ser aberto para leitura\n");
 		return 1;
 	}
@@ -39,35 +39,33 @@ int main(int argc, char **argv) {
 
 	rewind(f);
 
+  fseek(f, meio * sizeof(Endereco), SEEK_SET);
+  fread(&e, sizeof(Endereco), 1, f);
+
+  if(strncmp(argv[1],e.cep,8) == 0){
+  	cont = 1;
+    ultimo = primeiro - 1;
+	}
+
+	while(primeiro <= ultimo ){
+  	cont++;
+    meio = (primeiro+ultimo)/2;
+
     fseek(f, meio * sizeof(Endereco), SEEK_SET);
     fread(&e, sizeof(Endereco), 1, f);
 
-    if(strncmp(argv[1],e.cep,8) == 0){
-        cont = 1;
-        ultimo = primeiro - 1;
+    if (strncmp(argv[1],e.cep,8) == 0){
+    	break;
+		}
+    if(strncmp(argv[1],e.cep,8) > 0){
+	    primeiro = meio + 1;
+    }else{
+  	  ultimo = meio - 1;
     }
-
-    while(primeiro <= ultimo ){
-        cont++;
-        meio = (primeiro+ultimo)/2;
-
-        fseek(f, meio * sizeof(Endereco), SEEK_SET);
-        fread(&e, sizeof(Endereco), 1, f);
-
-        if (strncmp(argv[1],e.cep,8) == 0){
-            break;
-        }
-
-        if(strncmp(argv[1],e.cep,8) > 0){
-            primeiro = meio + 1;
-        }else{
-            ultimo = meio - 1;
-        }
-
-    }
-    if(strncmp(argv[1],e.cep,8) == 0){
-        printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
-    }
+	}
+  if(strncmp(argv[1],e.cep,8) == 0){
+  	printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+  }
 	printf("Contador: %d \n", cont);
 	fclose(f);
 	return 0;
